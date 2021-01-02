59214ba3f92af83c9491d51000cfaec431aac5b8
uedi <uedi@users.noreply.github.com>
2021-01-02 18:19:32 +0200

    configure, install packages for navigation

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 import Main from './src/Main'
 
 const App = () => {
     return (
-        <Main />
+        <NavigationContainer>
+            <Main />
+        </NavigationContainer>
     )
 }
 
